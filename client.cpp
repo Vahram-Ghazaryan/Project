e22339152138417a8486eca48243443a7d73beeb
Aram Arakelyan <aramarakelyan15797@gmail.com>
2024-08-21 14:54:52 +0400

    send and receive file -

--- client.cpp
+++ client.cpp
@@ -9,6 +9,8 @@
 #include <sstream>
 #include <atomic>
 #include <mutex>
+#include <fstream>
+#include <functional>
 
 using boost::asio::ip::tcp;
 void connect_to_client(const std::string& client_ip, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username);
@@ -94,13 +96,17 @@ void handle_read(std::shared_ptr<tcp::socket> socket, std::shared_ptr<tcp::socke
             } else if (response == "reject") {
                 std::cout << "Connection request rejected by the other client." << std::endl;
                 notify_server_status(server_socket, "waiting", username);
-                boost::asio::async_write(*socket, boost::asio::buffer("wait " + username), handle_write);
             } else if (response.find("IP: ") == 0) {
                 std::string client_ip = response.substr(4);
                 std::cout << "IP address of the selected client: " << client_ip << std::endl;
                 connect_to_client(client_ip, io_context, server_socket, username);
             } else {
-                std::cout << "Online clients:\n" << response << std::endl;
+            	std::string text;
+            	if (response.find("ip not found") == 0){
+            		text = "Username not found.";
+            	} else text = "Online clients:\n";
+            	
+                std::cout << text << response << std::endl;
 
                 std::string target_username;
                 std::cout << "Enter the username of the client you want to connect to (or 'wait' to wait for incoming connections): ";
@@ -108,7 +114,7 @@ void handle_read(std::shared_ptr<tcp::socket> socket, std::shared_ptr<tcp::socke
 
                 if (target_username == "wait") {
                     std::cout << "To return to the previous menu you can restart the program\nSwitching to standby mode.." << std::endl;
-                    boost::asio::async_write(*socket, boost::asio::buffer("wait " + username), handle_write);
+                    notify_server_status(server_socket, "waiting", username);
                 } else {
                 	std::cout << "Wait for the user to accept the connection.." << std::endl;
                     std::string connect_message = "connect " + target_username;
@@ -139,33 +145,99 @@ void handle_read(std::shared_ptr<tcp::socket> socket, std::shared_ptr<tcp::socke
     });
 }
 
+void send_file(const std::string& path, std::shared_ptr<tcp::socket> client_socket) {
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << path << std::endl;
+        return;
+    }
+    
+    std::streamsize file_size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    auto buffer = std::make_shared<std::vector<char>>(1024);
+
+    std::function<void(boost::system::error_code, std::size_t)> write_handler;
+    write_handler = [buffer, client_socket, &file, write_handler](boost::system::error_code ec, std::size_t /*length*/) mutable {
+        if (ec) {
+            std::cerr << "Error during file transfer: " << ec.message() << std::endl;
+            return;
+        }
+        if (file) {
+            if (file.read(buffer->data(), buffer->size()) || file.gcount() > 0) {
+                boost::asio::async_write(*client_socket, boost::asio::buffer(buffer->data(), file.gcount()), write_handler);
+            } else {
+                std::cerr << "Error reading from file " << std::endl;
+            }
+        } else {
+            std::cout << "File transfer completed successfully." << std::endl;
+        }
+    };
+
+    if (file.read(buffer->data(), buffer->size()) || file.gcount() > 0) {
+        boost::asio::async_write(*client_socket, boost::asio::buffer(buffer->data(), file.gcount()), write_handler);
+    } else {
+        std::cerr << "Error reading from file " << std::endl;
+    }
+}
+
+
+void receive_file(const std::string& path, std::shared_ptr<tcp::socket> client_socket) {
+    std::ofstream file(path, std::ios::binary);
+    if (!file) {
+        std::cout << "Error creating file: " << path << std::endl;
+        return;
+    }
+
+    auto buffer = std::make_shared<std::vector<char>>(1024);
+
+    std::function<void(boost::system::error_code, std::size_t)> read_handler;
+    read_handler = [buffer, client_socket, &file, read_handler](boost::system::error_code ec, std::size_t length) mutable {
+        if (!ec && length > 0) {
+            file.write(buffer->data(), length);
+            client_socket->async_read_some(boost::asio::buffer(*buffer), read_handler);
+        } else if (ec) {
+            std::cout << "File reception failed: " << ec.message() << std::endl;
+        } else {
+            std::cout << "File reception completed." << std::endl;
+            file.close();
+        }
+    };
+
+    client_socket->async_read_some(boost::asio::buffer(*buffer), read_handler);
+}
+
 void start_chat(std::shared_ptr<tcp::socket> client_socket, std::shared_ptr<tcp::socket> server_socket, const std::string& username) {
-	const std::string user_color = "\033[34m";  // Blue
-    const std::string reset_color = "\033[0m";  // Reset to default	
-	
+    const std::string user_color = "\033[34m";  // Blue
+    const std::string reset_color = "\033[0m";  // Reset to default
+
     std::atomic<bool> stop_chatting{false};
     auto buffer = std::make_shared<std::array<char, 1024>>();
-	
+
     std::function<void(const boost::system::error_code&, std::size_t)> read_handler;
-	
+
     read_handler = [client_socket, server_socket, buffer, username, &stop_chatting, &read_handler](const boost::system::error_code& error, std::size_t length) mutable {
         if (!error) {
             std::string message(buffer->data(), length);
             if (message.find("/disconnect") == 0) {
                 client_socket->close();
                 stop_chatting = true;
-                std::cout << "The other client has disconnected\nPress enter to continue." << std::endl;    
+                std::cout << "The other client has disconnected\nPress enter to continue." << std::endl;
                 return;
+            } else if (message.find("/file") == 0) {
+                std::string path = "received_" + message.substr(6); // Extract the filename from the message
+                std::cout << "Receiving file: " << path << std::endl;
+                receive_file(path, client_socket);
             } else {
-                print_message(username, message, false);
+                std::cout << "Message from other client: " << message << std::endl;
                 client_socket->async_read_some(boost::asio::buffer(*buffer), read_handler);
             }
         } else {
-        	std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        	if (!stop_chatting) {
-        		std::cout << "The other client closed the program incorrectly\nPress enter to continue." << std::endl;
-        		stop_chatting = true;
-            	return;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            if (!stop_chatting) {
+                std::cout << "The other client closed the program incorrectly\nPress enter to continue." << std::endl;
+                stop_chatting = true;
+                return;
             }
         }
     };
@@ -173,39 +245,38 @@ void start_chat(std::shared_ptr<tcp::socket> client_socket, std::shared_ptr<tcp:
     client_socket->async_read_some(boost::asio::buffer(*buffer), read_handler);
 
     while (true) {
-
-    std::string message;
-    std::cout << user_color << "You\n-----------" << reset_color << std::endl;
-    std::getline(std::cin, message);
-    if (stop_chatting) {
-    	notify_server_status(server_socket, "free", username);
-        break;
-    }
-    if (message == "/disconnect") {
-        std::cout << "Disconnecting chat." << std::endl;
-        boost::asio::io_context io_context;
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        boost::asio::async_write(*client_socket, boost::asio::buffer("/disconnect"), handle_write);
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        client_socket->close();
-        stop_chatting = true;
-        notify_server_status(server_socket, "free", username);
-        break;
-    } else if (message == "/exit") {
-        std::cout << "Exiting program." << std::endl;
-        boost::asio::async_write(*client_socket, boost::asio::buffer("/disconnect"), handle_write);
-        client_socket->close();
-        notify_server_status(server_socket, "offline", username);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        exit(0);
-    } else if (!stop_chatting) {
-        std::string full_message = username + ": " + message;
-        boost::asio::async_write(*client_socket, boost::asio::buffer(full_message), handle_write);
-        print_message("You", message, true);
+        std::string message;
+        std::cout << user_color << "You\n-----------" << reset_color << std::endl;
+        std::getline(std::cin, message);
+        if (stop_chatting) {
+            notify_server_status(server_socket, "free", username);
+            break;
+        }
+        if (message == "/disconnect") {
+            std::cout << "Disconnecting chat." << std::endl;
+            boost::asio::async_write(*client_socket, boost::asio::buffer("/disconnect"), handle_write);
+            client_socket->close();
+            stop_chatting = true;
+            notify_server_status(server_socket, "free", username);
+            break;
+        } else if (message == "/exit") {
+            std::cout << "Exiting program." << std::endl;
+            boost::asio::async_write(*client_socket, boost::asio::buffer("/disconnect"), handle_write);
+            client_socket->close();
+            notify_server_status(server_socket, "offline", username);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            exit(0);
+        } else if (message.find("/file") == 0) {
+            std::string path = message.substr(6); // Extract the path from the command
+            send_file(path, client_socket);
+        } else if (!stop_chatting) {
+            std::string full_message = username + ": " + message;
+            boost::asio::async_write(*client_socket, boost::asio::buffer(full_message), handle_write);
+            print_message("You", message, true);
+        }
     }
 }
 
-}
 
 
 
