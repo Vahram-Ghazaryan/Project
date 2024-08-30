1b80136aeb73a4629f2d29ac175985175f96cff2
Aram Arakelyan <aramarakelyan15797@gmail.com>
2024-08-30 20:22:58 +0400

    fix /disconnect..

--- client.cpp
+++ client.cpp
@@ -42,9 +42,8 @@ std::string replace_emojis(const std::string& message) {
     }
     return result;
 }
-bool connected = false;
 
-void connect_to_client(const std::string& client_ip, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username);
+void connect_to_client(const std::string& client_ip, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username, std::shared_ptr<std::atomic<bool>> connected_ptr);
 
 std::string current_time() {
     auto now = std::chrono::system_clock::now();
@@ -106,29 +105,28 @@ void request_list(std::shared_ptr<tcp::socket> server_socket, const std::string&
         }
     });
 }
-std::atomic<bool> acception(false);
 
 void handle_read(std::shared_ptr<tcp::socket> socket, 
                  std::shared_ptr<tcp::socket> server_socket, 
                  boost::asio::io_context& io_context, 
                  const std::string& username, 
-                 std::shared_ptr<std::atomic<bool>> acception) {
+                 std::shared_ptr<std::atomic<bool>> connected_ptr) {
     
     auto buffer = std::make_shared<std::array<char, 1024>>();
 
     socket->async_read_some(boost::asio::buffer(*buffer), 
-    [socket, server_socket, buffer, &io_context, username, acception]
+    [socket, server_socket, buffer, &io_context, username, connected_ptr]
     (const boost::system::error_code& error, std::size_t length) {
         if (!error) {
             static std::unordered_map<std::string, bool> clients_list;
             static std::unordered_map<std::string, std::string> clients_username_ip;
                 std::string response(buffer->data(), length);
-//                std::thread([username, server_socket, response, socket, &io_context, acception]() {        	
-                if (connected) {
+
+//                std::thread([username, server_socket, response, socket, &io_context]() {        	
+                if (connected_ptr -> load()) {
                    return; 
                 }
                 std::fstream file;
-                acception -> store(true);
                 file.open("online_clinets.txt", std::ios::in | std::ios::out | std::ios::trunc);
                 if (file.is_open()) {
                     if (response.find("There is no online user") == std::string::npos && response.size() > 35) {
@@ -171,8 +169,8 @@ void handle_read(std::shared_ptr<tcp::socket> socket,
                     std::cerr << "Error during open file\n";
                 }
   //          }).detach();
-                    std::thread([username, server_socket, response, socket, &io_context, acception]() { 
-                    if (connected) {
+                    std::thread([username, server_socket, response, socket, &io_context, connected_ptr]() { 
+                    if (connected_ptr -> load()) {
                         return;
                     }
                     std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -180,7 +178,7 @@ void handle_read(std::shared_ptr<tcp::socket> socket,
                     std::string target_username;
                     std::getline(std::cin, target_username);
                     while (clients_list.find(target_username) == clients_list.end() || clients_list[target_username] == false) {
-                        if (connected) {
+                        if (connected_ptr -> load()) {
                             return;
                         }
                         if (clients_list.find(target_username) == clients_list.end()) { 
@@ -191,14 +189,14 @@ void handle_read(std::shared_ptr<tcp::socket> socket,
                             std::getline(std::cin, target_username);
                     }                  
                     std::cout << "IP address of the selected client: " << clients_username_ip[target_username] << std::endl;
-                    connect_to_client(clients_username_ip[target_username], io_context, server_socket, username);
+                    connect_to_client(clients_username_ip[target_username], io_context, server_socket, username, connected_ptr);
                     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                      
+                    return;  
                 }).detach();
                 
             
 
-            handle_read(socket, server_socket, io_context, username, acception);
+            handle_read(socket, server_socket, io_context, username, connected_ptr);
         } else {
             std::cerr << "Error during read: " << error.message() << "\n";
         }
@@ -344,23 +342,25 @@ void receive_file_multithreaded(boost::asio::ip::tcp::socket& socket, const std:
     }
 }
 
-void start_chat(std::shared_ptr<tcp::socket> client_socket, std::shared_ptr<tcp::socket> server_socket, const std::string& username) {
+void start_chat(std::shared_ptr<tcp::socket> client_socket, std::shared_ptr<tcp::socket> server_socket, const std::string& username, std::shared_ptr<std::atomic<bool>> connected_ptr) {
     const std::string user_color = "\033[34m";  // Blue
     const std::string reset_color = "\033[0m";  // Reset to default
     std::atomic<bool> receive(false);
     std::atomic<bool> stop_chatting{false};
     auto buffer = std::make_shared<std::array<char, 1024>>();
-
+	boost::asio::io_context io_context;
     std::function<void(const boost::system::error_code&, std::size_t)> read_handler;
 
-    read_handler = [client_socket, server_socket, buffer, username, &stop_chatting, &read_handler, &receive](const boost::system::error_code& error, std::size_t length) mutable {
+    read_handler = [client_socket, server_socket, buffer, username, &stop_chatting, &read_handler, &receive, connected_ptr, &io_context](const boost::system::error_code& error, std::size_t length) mutable {
         if (!error) {
             std::string message(buffer->data(), length);
             if (message.find("/disconnect") == 0) {
                 client_socket->close();
                 stop_chatting = true;
-                connected = false;
+                connected_ptr -> store(false);
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 notify_server_status(server_socket, "free", username);
+                handle_read(server_socket, server_socket, io_context, username, connected_ptr);
                 std::cout << "The other client has disconnected\nPress enter to continue." << std::endl;
                 return;
             } else if (message.find("/file") == 0) {
@@ -399,15 +399,18 @@ void start_chat(std::shared_ptr<tcp::socket> client_socket, std::shared_ptr<tcp:
         if (message == "/disconnect") {
             std::cout << "Disconnecting chat." << std::endl;
             boost::asio::async_write(*client_socket, boost::asio::buffer("/disconnect"), handle_write);
+            client_socket->cancel();
             client_socket->close();
             stop_chatting = true;
-            connected = false;
+            connected_ptr -> store(false);
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
             notify_server_status(server_socket, "free", username);
-            
+            handle_read(server_socket, server_socket, io_context, username, connected_ptr);          
             break;
         } else if (message == "/exit") {
             std::cout << "Exiting program." << std::endl;
             boost::asio::async_write(*client_socket, boost::asio::buffer("/disconnect"), handle_write);
+            client_socket->cancel();
             client_socket->close();
             notify_server_status(server_socket, "offline", username);
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -437,21 +440,21 @@ void start_chat(std::shared_ptr<tcp::socket> client_socket, std::shared_ptr<tcp:
 
 
 
-void accept_connections(std::shared_ptr<tcp::acceptor> acceptor, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username) {
+void accept_connections(std::shared_ptr<tcp::acceptor> acceptor, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username, std::shared_ptr<std::atomic<bool>> connected_ptr) {
     auto new_socket = std::make_shared<tcp::socket>(io_context);
-    acceptor->async_accept(*new_socket, [new_socket, acceptor, &io_context, server_socket, username](const boost::system::error_code& error) {
+    acceptor->async_accept(*new_socket, [new_socket, acceptor, &io_context, server_socket, username, connected_ptr](const boost::system::error_code& error) {
         if (!error) {
         	std::cout << "Use /disconnect to disconnect from the other person and /exit to exit the program\nYou can also use these emojis :smile :sad :laugh :angry :wink :heart" << std::endl;
             notify_server_status(server_socket, "no free", username);
-            connected = true;
-            std::thread chat_thread(start_chat, new_socket, server_socket, username);
+            connected_ptr -> store(true);
+            std::thread chat_thread(start_chat, new_socket, server_socket, username, connected_ptr);
             chat_thread.detach();
 /*            auto buffer = std::make_shared<std::array<char, 1024>>();
             new_socket -> async_read_some(boost::asio::buffer(*buffer), [buffer, server_socket, username, new_socket] (const boost::system::error_code& error, std::size_t length) {
                 if (!error) {
                     std::string response(buffer -> data());
                     if (response.find("request") != std::string::npos) {
-                        connected = true;
+                        connected_ptr -> store(true);
                         std::string reply;
                         std::getline(std::cin, reply);
                         while (!(reply == "accept" || reply == "reject")) {
@@ -474,26 +477,26 @@ void accept_connections(std::shared_ptr<tcp::acceptor> acceptor, boost::asio::io
             });*/
         } else {
             std::cerr << "Error during accept: " << error.message() << "\n";
-            auto acception_ptr = std::make_shared<std::atomic<bool>> (false);
-            handle_read(server_socket, server_socket, io_context, username, acception_ptr);
+          
+            handle_read(server_socket, server_socket, io_context, username, connected_ptr);
         }
 
-        accept_connections(acceptor, io_context, server_socket, username);
+        accept_connections(acceptor, io_context, server_socket, username, connected_ptr);
     });
 }
 
-void connect_to_client(const std::string& client_ip, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username) {
+void connect_to_client(const std::string& client_ip, boost::asio::io_context& io_context, std::shared_ptr<tcp::socket> server_socket, const std::string& username, std::shared_ptr<std::atomic<bool>> connected_ptr) {
     auto client_socket = std::make_shared<tcp::socket>(io_context);
     tcp::resolver resolver(io_context);
     tcp::resolver::results_type client_endpoints = resolver.resolve(client_ip, "12347");
 
-    boost::asio::async_connect(*client_socket, client_endpoints, [client_socket, server_socket, username, &io_context](const boost::system::error_code& error, const tcp::endpoint&) {
+    boost::asio::async_connect(*client_socket, client_endpoints, [client_socket, server_socket, username, &io_context, connected_ptr](const boost::system::error_code& error, const tcp::endpoint&) {
         if (!error) {
             std::cout << "Connected to client. You can now start chatting.\nUse /disconnect to disconnect from the other person and /exit to exit the program\nYou can also use these emojis :smile :sad :laugh :angry :wink :heart" << std::endl;
             notify_server_status(server_socket, "no free", username);
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            connected =  true;
-            std::thread chat_thread(start_chat, client_socket, server_socket, username);
+            connected_ptr -> store(true);
+            std::thread chat_thread(start_chat, client_socket, server_socket, username, connected_ptr);
             chat_thread.detach();
  /*           boost::asio::async_write(*client_socket, boost::asio::buffer("request " + username), handle_write);
             auto buffer = std::make_shared<std::array<char, 1024>>();	
@@ -513,9 +516,8 @@ void connect_to_client(const std::string& client_ip, boost::asio::io_context& io
                 }
             });*/
         } else {
-            std::cerr << "Error during client connection: " << error.message() << "\n";
-            auto acception_ptr = std::make_shared<std::atomic<bool>> (false);
-            handle_read(server_socket, server_socket, io_context, username, acception_ptr);
+            std::cerr << "Error during client connection: " << error.message() << "\n";            
+            handle_read(server_socket, server_socket, io_context, username, connected_ptr);
         }
     });
 }
@@ -539,10 +541,8 @@ std::pair<std::string, std::string> read_config(const std::string& filename) {
 
     return {host, port};
 }
-void print_welcome_message() {
-	std::cout << "𝕎 𝔼 𝕃 ℂ 𝕆 𝕄 𝔼  𝕋 𝕆  𝕃 𝕀ℕ 𝕌 𝕏  ℂ ℍ 𝔸 𝕋\n";
 	
-}
+
 int main(int argc, char* argv[]) {	
     try {
         if (argc != 2) { 
@@ -573,14 +573,14 @@ int main(int argc, char* argv[]) {
                 boost::asio::async_write(*server_socket, boost::asio::buffer(buffer), [server_socket, &io_context, username](const boost::system::error_code& error, std::size_t length) {
                     handle_write(error, length);
 
-                    auto acceptor = std::make_shared<tcp::acceptor>(io_context, tcp::endpoint(tcp::v4(), 12347)); // Port for accepting connections
-                    std::thread accept_thread(accept_connections, acceptor, std::ref(io_context), server_socket, username);
-                    accept_thread.detach();
+                    auto acceptor = std::make_shared<tcp::acceptor>(io_context, tcp::endpoint(tcp::v4(), 12347)); // Port for accepting connections                    
+                    auto connected_ptr = std::make_shared<std::atomic<bool>>(false);
                     
-                    auto acception_ptr = std::make_shared<std::atomic<bool>>(false);
-
-                    std::thread handle_thread([server_socket, &io_context, username, acception_ptr]() {
-                        handle_read(server_socket, server_socket, io_context, username, acception_ptr);
+                    std::thread accept_thread(accept_connections, acceptor, std::ref(io_context), server_socket, username, connected_ptr);
+                    accept_thread.detach();
+                                       
+                    std::thread handle_thread([server_socket, &io_context, username, connected_ptr]() {
+                        handle_read(server_socket, server_socket, io_context, username, connected_ptr);
                     });
                     handle_thread.detach();
 
